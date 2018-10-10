271a56ebea769dcd640edb319dd4f4a5f2c88ae6
jzyhywxz <jzyhywxz4zzw@gmail.com>
2018-10-10 09:27:09 +0800

    Updating the QueryEntry and MutationEntry

--- src/main/java/com/zzw/github/graphql/schema/mutations/MutationEntry.java
+++ src/main/java/com/zzw/github/graphql/schema/mutations/MutationEntry.java
@@ -3,10 +3,18 @@ package com.zzw.github.graphql.schema.mutations;
 public class MutationEntry {
     private Mutation data;
     private com.zzw.github.graphql.schema.Error[] errors;
+    private String documentation_url;
+    private String message;
 
     public Mutation getData() { return data; }
     public void setData(Mutation data) { this.data = data; }
 
     public com.zzw.github.graphql.schema.Error[] getErrors() { return errors; }
     public void setErrors(com.zzw.github.graphql.schema.Error[] errors) { this.errors = errors; }
+
+    public String getDocumentation_url() { return documentation_url; }
+    public void setDocumentation_url(String documentation_url) { this.documentation_url = documentation_url; }
+
+    public String getMessage() { return message; }
+    public void setMessage(String message) { this.message = message; }
 }
